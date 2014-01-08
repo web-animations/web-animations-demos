6b10cac2e862761d4c845cc8af3edb2239eba642
Bugs Nash <bugsnash@google.com>
2014-01-08 17:32:30 +1100

    Polymerised the stick-person.
    Moved stick-person html structure to new file stick-person.html and changed index.html to use the polymer element stick-person.
    
    Adjusted the javascript file for a Polymer element.
    
    Changed the background to white and the stick-person to black for generalisability.
    
    R=mikelawther@chromium.org
    
    Review URL: https://codereview.appspot.com/48400043

--- walking/stick-person.js
+++ walking/stick-person.js
@@ -1,20 +1,24 @@
-var timingLimbs = {direction: 'alternate', duration: 1.5, iterations: 2};
-var anim = new ParGroup([
-    new Animation(document.getElementById('bruce'), [{transform: 'translateY(15px)'}, {transform: 'translateY(0px)'}], {direction: 'alternate', duration: 0.75, iterations: 4}),
+Polymer('stick-person', {
+    ready: function() {
+        var timingLimbs = {direction: 'alternate', duration: 1.5, iterations: 2};
+        var anim = new ParGroup([
+            new Animation(this.$.bruce, [{transform: 'translateY(15px)'}, {transform: 'translateY(0px)'}], {direction: 'alternate', duration: 0.75, iterations: 4}),
 
-    /* Arms */
-    new Animation(document.getElementById('leftArm'), [{transform: 'rotate(-40deg)'}, {transform: 'rotate(53deg)'}], timingLimbs),
-    new Animation(document.getElementById('leftForearm'), [{transform: 'rotate(-30deg)'}, {transform: 'rotate(0deg)'}], timingLimbs),
-    new Animation(document.getElementById('rightArm'), [{transform: 'rotate(53deg)'}, {transform: 'rotate(-40deg)'}], timingLimbs),
-    new Animation(document.getElementById('rightForearm'), [{transform: 'rotate(0deg)'}, {transform: 'rotate(-30deg)'}], timingLimbs),
+            /* Arms */
+            new Animation(this.$.leftArm, [{transform: 'rotate(-40deg)'}, {transform: 'rotate(53deg)'}], timingLimbs),
+            new Animation(this.$.leftForearm, [{transform: 'rotate(-30deg)'}, {transform: 'rotate(0deg)'}], timingLimbs),
+            new Animation(this.$.rightArm, [{transform: 'rotate(53deg)'}, {transform: 'rotate(-40deg)'}], timingLimbs),
+            new Animation(this.$.rightForearm, [{transform: 'rotate(0deg)'}, {transform: 'rotate(-30deg)'}], timingLimbs),
 
-    /* Legs */
-    new Animation(document.getElementById('leftLeg'), [{transform: 'rotate(35deg)'}, {transform: 'rotate(-30deg)'}], timingLimbs),
-    new Animation(document.getElementById('leftShin'), [{transform: 'rotate(0deg)'}, {transform: 'rotate(20deg)'}], timingLimbs),
-    new Animation(document.getElementById('leftFoot'), [{transform: 'rotate(-110deg)'}, {transform: 'rotate(-90deg)'}], timingLimbs),
-    new Animation(document.getElementById('rightLeg'), [{transform: 'rotate(-30deg)'}, {transform: 'rotate(35deg)'}], timingLimbs),
-    new Animation(document.getElementById('rightShin'), [{transform: 'rotate(20deg)'}, {transform: 'rotate(0deg)'}], timingLimbs),
-    new Animation(document.getElementById('rightFoot'), [{transform: 'rotate(-90deg)'}, {transform: 'rotate(-110deg)'}], timingLimbs)
-], {iterations: Infinity});
+            /* Legs */
+            new Animation(this.$.leftLeg, [{transform: 'rotate(35deg)'}, {transform: 'rotate(-30deg)'}], timingLimbs),
+            new Animation(this.$.leftShin, [{transform: 'rotate(0deg)'}, {transform: 'rotate(20deg)'}], timingLimbs),
+            new Animation(this.$.leftFoot, [{transform: 'rotate(-110deg)'}, {transform: 'rotate(-90deg)'}], timingLimbs),
+            new Animation(this.$.rightLeg, [{transform: 'rotate(-30deg)'}, {transform: 'rotate(35deg)'}], timingLimbs),
+            new Animation(this.$.rightShin, [{transform: 'rotate(20deg)'}, {transform: 'rotate(0deg)'}], timingLimbs),
+            new Animation(this.$.rightFoot, [{transform: 'rotate(-90deg)'}, {transform: 'rotate(-110deg)'}], timingLimbs)
+        ], {iterations: Infinity});
 
-document.timeline.play(anim);
+        document.timeline.play(anim);
+    }
+});
