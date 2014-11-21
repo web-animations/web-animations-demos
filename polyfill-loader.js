4d9102e87f9f782401e1dce61b1b0b02565eccac
Douglas Stockwell <dstockwell@chromium.org>
2014-11-21 22:26:25 +1100

    Switch to the new web-animations-js

--- polyfill-loader.js
+++ polyfill-loader.js
@@ -1,3 +1,3 @@
 if (window.location.search.indexOf('nopolyfill') < 0) {
-  document.write('<script src="../components/web-animations-js-staging/web-animations-next.min.js"></script>');
+  document.write('<script src="../components/web-animations-js/web-animations-next.min.js"></script>');
 }
